7e790e9b4e1f68f074f891b048df19880a113011
Gio on ArchXPS <github@gpiantoni.com>
2018-01-13 15:46:36 +0100

    feat runs all the way anyway

--- tests/test_09_feat_finished.py
+++ tests/test_09_feat_finished.py
@@ -8,10 +8,4 @@ from boavus.fmri.percent import percent_fmri
 def test_fmri_percent():
     if environ.get('FSLDIR') is not None:
         feat_path = Path('tests/data/derivatives/feat/sub-bert/ses-day01/func/sub-bert_ses-day01_task-block_run-00.feat')
-        while True:
-            if (feat_path / 'rendered_thresh_zstat1.png').exists():
-                break
-
-            sleep(1)
-
         percent_fmri(feat_path)
