895e966b8b1b0704804f13a2e783e10bdc964994
Gio on ArchXPS <github@gpiantoni.com>
2018-01-13 17:16:30 +0100

    run coreg

--- tests/test_09_feat_finished.py
+++ tests/test_09_feat_finished.py
@@ -3,9 +3,17 @@ from time import sleep
 
 from os import environ
 from boavus.fmri.percent import percent_fmri
+from boavus.fmri.feat import coreg_feat2freesurfer
+from .paths import FREESURFER_PATH
+
+feat_path = Path('tests/data/derivatives/feat/sub-bert/ses-day01/func/sub-bert_ses-day01_task-block_run-00.feat')
 
 
 def test_fmri_percent():
     if environ.get('FSLDIR') is not None:
-        feat_path = Path('tests/data/derivatives/feat/sub-bert/ses-day01/func/sub-bert_ses-day01_task-block_run-00.feat')
         percent_fmri(feat_path)
+
+
+def test_feat_freesurfer():
+    if environ.get('FSLDIR') is not None:
+        coreg_feat2freesurfer(feat_path, FREESURFER_PATH)
