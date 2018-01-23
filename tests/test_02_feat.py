1faac2c0754419c1a3d7cca2c7ba0280ea8c2fc4
Gio on ArchXPS <github@gpiantoni.com>
2018-01-23 07:36:51 +0100

    run fsl

--- tests/test_02_feat.py
+++ tests/test_02_feat.py
@@ -1,14 +1,11 @@
 from os import environ
 
-from bidso import Task
-from boavus.fsl.feat import run_feat
+from boavus.fsl.feat import run_fsl_feat
 
 from .paths import BIDS_PATH, FEAT_PATH
-from .test_01_simulate import task_fmri
 
 
 def test_run_feat_dryrun():
-    t = Task(task_fmri.get_filename(BIDS_PATH))
 
     if environ.get('FSLDIR') is not None:
-        run_feat(FEAT_PATH, t, dry_run=False)
+        run_fsl_feat(BIDS_PATH, FEAT_PATH)
