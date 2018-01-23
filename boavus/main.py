74a219f86a61d82da56e3d0f12de2d14cb0d1e37
Gio at UMCU <github@gpiantoni.com>
2018-01-23 10:52:17 +0100

    use a simpler explanation for boavus main

--- boavus/main.py
+++ boavus/main.py
@@ -1,4 +1,4 @@
-from argparse import ArgumentParser, RawTextHelpFormatter
+from argparse import ArgumentParser, RawTextHelpFormatter, RawDescriptionHelpFormatter
 from logging import getLogger, StreamHandler, Formatter, INFO, DEBUG
 from pathlib import Path
 
@@ -20,44 +20,81 @@ STEPS = [
     'ieeg_electrodes',
     ]
 
-
-parser = ArgumentParser(description='Tools to analyze data structured as BIDS in Python', formatter_class=RawTextHelpFormatter)
-"""
-subparsers = parser.add_subparsers(title='subcommands', help='sub-command help')
-
-parser_a = subparsers.add_parser('fsl_feat', help='a help')
-parser_a.set_defaults(func='fsl_feat')
-arg_feat_dir = {'name':'--feat_dir', 'help':'The directory with FSL/feat'}
-
-parser_a.add_argument(**arg_feat_dir)
-
-parser_b = subparsers.add_parser('fmri_percent', help='a help')
-
-parser_c = subparsers.add_parser('ieeg_corrfmri', help='a help')
-
-"""
-parser.add_argument('bids_dir', help='The directory with the input dataset '
-                    'formatted according to the BIDS standard.')
-parser.add_argument(
-    'steps', nargs='+', choices=STEPS, metavar='step',
-    help='fsl_feat: run feat (requires feat_dir)\n'
-         'fmri_percent: compute the percent change (requires feat_dir, output_dir)\n'
-         'ieeg_electrodes: project electrodes to surface (requires bids_dir, freesurfer_dir)\n'
-         'ieeg_corrfmri: find kernel size for iEEG based on fMRI (requires feat_dir, freesurfer_dir, output_dir)\n'
+args = dict(
+    fsl=dict(
+        help='test fsl',
+        feat=dict(
+            help='run FEAT using the events.tsv information',
+            arguments=[
+                'bids_dir',
+                'feat_dir',
+                ],
+            ),
+        ),
+    fmri=dict(
+        help='test fmri',
+        percent=dict(
+            help='compute percent change of the BOLD signal',
+            arguments=[
+                'feat_dir',
+                'output_dir',
+                ]
+            ),
+        ),
+    ieeg=dict(
+        help='test ieeg',
+        electrodes=dict(
+            help='project electrodes in fMRI and assign them regions',
+            arguments=[
+                'bids_dir',
+                'freesurfer_dir',
+                ]
+            ),
+        corrfmri=dict(
+            help='find best kernel size for iEEG electrodes based on fMRI',
+            arguments=[
+                'bids_dir',
+                'feat_dir',
+                'freesurfer_dir',
+                'output_dir',
+                ]
+            ),
+        )
     )
 
-parser.add_argument(
-    '--feat_dir',
-    help='The directory with FSL/feat')
-parser.add_argument(
-    '-f', '--freesurfer_dir',
-    help='The directory with Freesurfer')
-parser.add_argument(
-    '-o', '--output_dir',
-    help='The directory with custom output')
-parser.add_argument(
-    '-l', '--log', default='info',
-    help='Logging level: info (default), debug')
+
+parser = ArgumentParser(description='Tools to analyze data structured as BIDS in Python', formatter_class=RawTextHelpFormatter)
+list_modules = parser.add_subparsers(title='Modules', help='Modules containing the functions')
+
+for m_k, m_v in args.items():
+    module = list_modules.add_parser(m_k, help=m_v.pop('help'))
+    module.set_defaults(module=m_k)
+    list_functions = module.add_subparsers(title=f'Functions in {m_k} module')
+
+    for f_k, f_v in m_v.items():
+        function = list_functions.add_parser(f_k, help=f_v.pop('help'))
+        function.set_defaults(function=f_k)
+        function.add_argument('-l', '--log', default='info',
+                              help='Logging level: info (default), debug')
+
+        required = function.add_argument_group('required arguments')
+
+        for arg in f_v['arguments']:
+            if arg == 'bids_dir':
+                required.add_argument('--bids_dir', required=True,
+                                      help='test')
+
+            elif arg == 'freesurfer_dir':
+                required.add_argument('--freesurfer_dir', required=True,
+                                      help='The directory with Freesurfer')
+
+            elif arg == 'feat_dir':
+                required.add_argument('--feat_dir', required=True,
+                                      help='The directory with FSL/feat')
+
+            elif arg == 'output_dir':
+                required.add_argument('--output_dir', required=True,
+                                      help='The directory with custom output')
 
 
 args = parser.parse_args()
@@ -82,31 +119,33 @@ def main():
     lg.handlers = []
     lg.addHandler(handler)
 
-    print(args)
-    bids_dir = _path(args.bids_dir)
+    if args.module == 'fsl':
 
-    for step in args.steps:
-
-        if step == 'fsl_feat':
+        if args.function == 'feat':
+            bids_dir = _path(args.bids_dir)
             feat_dir = _path(args.feat_dir)
             run_fsl_feat(bids_dir, feat_dir)
 
-        if step == 'fmri_percent':
+    elif args.module == 'fmri':
+
+        if args.function == 'percent':
             feat_dir = _path(args.feat_dir)
             output_dir = _path(args.output_dir)
             run_fmri_percent(feat_dir, output_dir)
 
-        if step == 'ieeg_corrfmri':
+    elif args.module == 'ieeg':
+
+        if args.function == 'electrodes':
+            bids_dir = _path(args.bids_dir)
+            freesurfer_dir = _path(args.freesurfer_dir)
+            run_ieeg_electrodes(bids_dir, freesurfer_dir)
+
+        elif args.function == 'corrfmri':
+            bids_dir = _path(args.bids_dir)
             feat_dir = _path(args.feat_dir)
             freesurfer_dir = _path(args.freesurfer_dir)
             output_dir = _path(args.output_dir)
-
             run_ieeg_corrfmri(bids_dir, feat_dir, freesurfer_dir, output_dir)
-
-        if step == 'ieeg_electrodes':
-            freesurfer_dir = _path(args.freesurfer_dir)
-
-            run_ieeg_electrodes(bids_dir, freesurfer_dir)
 
 
 def _path(dirname):
