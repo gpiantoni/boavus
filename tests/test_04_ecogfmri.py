8d1b6dd4ba3db7354fc163c6170fd34f0ca36241
Gio at UMCU <github@gpiantoni.com>
2018-01-23 09:29:31 +0100

    add bold in .feat names

--- tests/test_04_ecogfmri.py
+++ tests/test_04_ecogfmri.py
@@ -1,11 +1,11 @@
 from boavus.ieeg.corr_fmri import _main_to_elec
-from bidso.utils import add_extension, remove_underscore
+from bidso.utils import replace_extension
 
 from .paths import FREESURFER_PATH, BOAVUS_PATH, task_ieeg, task_fmri, FEAT_PATH, BIDS_PATH
 
 
 def test_corr_ecogfmri():
-    feat_path = add_extension(remove_underscore(task_fmri.get_filename(FEAT_PATH)), '.feat')
+    feat_path = replace_extension(task_fmri.get_filename(FEAT_PATH), '.feat')
     ieeg_file = task_ieeg.get_filename(BIDS_PATH)
 
     KERNEL_SIZES = [5, ]
--- tests/test_09_feat_finished.py
+++ tests/test_09_feat_finished.py
@@ -2,11 +2,11 @@ from os import environ
 from boavus.fmri.percent import percent_fmri
 from boavus.fsl.feat import coreg_feat2freesurfer
 
-from bidso.utils import add_extension, remove_underscore
+from bidso.utils import replace_extension
 
 from .paths import FREESURFER_PATH, FEAT_PATH, task_fmri
 
-feat_path = add_extension(remove_underscore(task_fmri.get_filename(FEAT_PATH)), '.feat')
+feat_path = replace_extension(task_fmri.get_filename(FEAT_PATH), '.feat')
 
 
 def test_fmri_percent():
