13d1f79d934e1f69bc5978403117d3b953c72a9e
Gio at UMCU <github@gpiantoni.com>
2018-01-29 15:23:31 +0100

    plot percent changes

--- boavus/ieeg/preprocessing.py
+++ boavus/ieeg/preprocessing.py
@@ -1,8 +1,11 @@
 from wonambi.trans import select, montage, math, timefrequency, concatenate
 from logging import getLogger
 from numpy import mean, std
+from numpy import array, log10
+import plotly.graph_objs as go
 
 from boavus.ieeg.dataset import Dataset
+from .percent import percent_ecog
 
 lg = getLogger(__name__)
 
@@ -15,7 +18,180 @@ PARAMETERS = {
         'off': '48',
         'minimalduration': 20,
         },
+    'reject': {
+        'chan': {
+            'threshold_std': 3,
+            },
+        },
+    'spectrogram': {
+        'duration': 1,
+        'taper': 'dpss',
+        'frequency': [
+            60,
+            90,
+            ],
+        },
     }
+
+FREQ = (60, 90)
+
+
+def compute_frequency(filename):
+    dat_move, dat_rest = preprocess_ecog(filename)
+
+    hfa_move, freq_move = compute_freq(dat_move)
+    hfa_rest, freq_rest = compute_freq(dat_rest)
+
+    # it's compute here with all the electrodes (also the non active ones)
+    ecog_stats = percent_ecog(hfa_move, hfa_rest)
+
+    """
+    hfa_move, hfa_rest = _select_active(hfa_move, hfa_rest)
+    active_chan = hfa_move.chan[0]
+    """
+    active = ' (todo)'
+    all_fig = []
+    for chan in ecog_stats.chan[0]:
+        fig = plot_psd(chan, active, freq_move, freq_rest, ecog_stats)
+        all_fig.append(fig)
+
+    return all_fig
+
+
+def compute_freq(dat):
+    """Remove epochs which have very high activity in high-freq range, then
+    average over time (only high-freq range) and ALL the frequencies."""
+    TAPER = PARAMETERS['spectrogram']['taper']
+    DURATION = PARAMETERS['spectrogram']['duration']
+    FREQ = PARAMETERS['spectrogram']['frequency']
+
+    if TAPER == 'dpss':
+        dat = timefrequency(dat, method='spectrogram', taper='dpss',
+                            duration=DURATION,
+                            halfbandwidth=abs(FREQ[1] - FREQ[0]) / 2)
+        dat = concatenate(dat, axis='time')
+
+        FREQ_SELECT = (mean(FREQ) - 1 / (DURATION * 2),
+                       mean(FREQ) + 1 / (DURATION * 2))
+
+    else:
+        dat = timefrequency(dat, taper=TAPER, method='spectrogram',
+                            duration=DURATION)
+        dat = concatenate(dat, axis='time')
+        FREQ_SELECT = FREQ
+
+    # dat, lg = _remove_outliers(dat)
+
+    freq_f = math(dat, axis='time', operator_name='mean')
+    dat = select(dat, freq=FREQ_SELECT)
+    freq_t = math(dat, axis='freq', operator_name='mean')
+
+    return freq_t, freq_f
+
+
+def plot_psd(chan, active, freq_move, freq_rest, perc):
+    mean_move = math(select(freq_move, freq=FREQ), operator_name='mean', axis='freq')(trial=0, chan=chan)
+    mean_rest = math(select(freq_rest, freq=FREQ), operator_name='mean', axis='freq')(trial=0, chan=chan)
+
+    YAXIS_RANGE = array([-2, 2], dtype=float)
+    FONT_SIZE = (14, 18)
+
+    traces = [
+        go.Scatter(
+            x=freq_rest.freq[0],
+            y=freq_rest(chan=chan, trial=0),
+            name='rest',
+            line=dict(
+                color='red',
+                ),
+            ),
+        go.Scatter(
+            x=freq_move.freq[0],
+            y=freq_move(chan=chan, trial=0),
+            name='move',
+            line=dict(
+                color='green',
+                ),
+            ),
+        go.Scatter(
+            x=FREQ,
+            y=array([1, 1]) * mean_rest,
+            name='rest (mean)',
+            line=dict(
+                color='red',
+                dash='10px',
+                ),
+            ),
+        go.Scatter(
+            x=FREQ,
+            y=array([1, 1]) * mean_move,
+            name='move (mean)',
+            line=dict(
+                color='green',
+                dash='5px',
+                ),
+            ),
+        go.Scatter(
+            x=(FREQ[0], FREQ[0]),
+            y=10 ** YAXIS_RANGE,
+            line=dict(
+                color='gray',
+                dash='dot',
+                ),
+            showlegend=False,
+            ),
+        go.Scatter(
+            x=(FREQ[1], FREQ[1]),
+            y=10 ** YAXIS_RANGE,
+            line=dict(
+                color='gray',
+                dash='dot',
+                ),
+            showlegend=False,
+            ),
+        ]
+
+    layout = go.Layout(
+        title=chan + active,
+        xaxis=dict(
+            title='Hz',
+            titlefont=dict(
+                size=FONT_SIZE[1],
+                ),
+            range=(0, 100),
+            tickfont=dict(
+                size=FONT_SIZE[0],
+                ),
+            ),
+        yaxis=dict(
+            title='μV<sup>2</sup>/Hz',
+            titlefont=dict(
+                size=FONT_SIZE[1],
+                ),
+            type='log',
+            range=YAXIS_RANGE,
+            tickfont=dict(
+                size=FONT_SIZE[0],
+                ),
+            ),
+        legend=dict(
+            font=dict(
+                size=FONT_SIZE[1],
+                ),
+            ),
+        annotations=[
+            dict(
+                x=FREQ[1],
+                y=log10(mean_move),
+                ax=50,
+                standoff=2,
+                text=f'{perc(trial=0, chan=chan).item():0.3f}%',
+                font=dict(
+                    size=FONT_SIZE[1],
+                    ),
+                ), ]
+        )
+    return go.Figure(data=traces, layout=layout)
 
 
 def preprocess_ecog(filename):
@@ -43,22 +219,22 @@ def preprocess_ecog(filename):
     dat_move = run_montage(self, move_times, clean_labels)
     dat_rest = run_montage(self, rest_times, clean_labels)
 
-    if False:
-        clean_roi_labels = [x['name'] for x in self.channels.tsv if x['name'].startswith('grid')]
-        print(clean_roi_labels)
+    if len(PARAMETERS['regions']) > 0:
+        elec = self.electrodes
+        labels_in_roi = [x['name'] for x in elec.electrodes.tsv if x['region'] in PARAMETERS['regions']]
+        clean_roi_labels = [label for label in clean_labels if label in labels_in_roi]
+    else:
+        clean_roi_labels = labels_in_roi = clean_labels
 
-        dat_move = select(dat_move, chan=clean_roi_labels)
-        dat_rest = select(dat_rest, chan=clean_roi_labels)
+    dat_move = select(dat_move, chan=clean_roi_labels)
+    dat_rest = select(dat_rest, chan=clean_roi_labels)
 
-    hfa_move, freq_move = compute_freq(dat_move)
-    hfa_rest, freq_rest = compute_freq(dat_rest)
-
-    return hfa_move, hfa_rest
+    return dat_move, dat_rest
 
 
 def reject_channels(dat):
     dat_std = math(dat, operator_name='std', axis='time')
-    THRESHOLD = 3
+    THRESHOLD = PARAMETERS['reject']['chan']['threshold_std']
     x = dat_std.data[0]
     thres = [mean(x) + THRESHOLD * std(x)]
     clean_labels = list(dat_std.chan[0][dat_std.data[0] < thres])
@@ -68,37 +244,6 @@ def reject_channels(dat):
 def run_montage(d, times, chan):
     dat = d.read_data(begsam=times[0], endsam=times[1], chan=chan)
     return montage(dat, ref_to_avg=True)
-
-
-def compute_freq(dat):
-    """Remove epochs which have very high activity in high-freq range, then
-    average over time (only high-freq range) and ALL the frequencies."""
-    DURATION = 1
-    TAPER = 'dpss'
-    FREQ = (60, 90)
-
-    if TAPER == 'dpss':
-        dat = timefrequency(dat, method='spectrogram', taper='dpss',
-                            duration=DURATION,
-                            halfbandwidth=abs(FREQ[1] - FREQ[0]) / 2)
-        dat = concatenate(dat, axis='time')
-
-        FREQ_SELECT = (mean(FREQ) - 1 / (DURATION * 2),
-                       mean(FREQ) + 1 / (DURATION * 2))
-
-    else:
-        dat = timefrequency(dat, taper=TAPER, method='spectrogram',
-                            duration=DURATION)
-        dat = concatenate(dat, axis='time')
-        FREQ_SELECT = FREQ
-
-    # dat, lg = _remove_outliers(dat)
-
-    freq_f = math(dat, axis='time', operator_name='mean')
-    dat = select(dat, freq=FREQ_SELECT)
-    freq_t = math(dat, axis='freq', operator_name='mean')
-
-    return freq_t, freq_f
 
 
 def read_markers(d, marker_on, marker_off, minimalduration):
--- tests/test_04_ecog.py
+++ tests/test_04_ecog.py
@@ -1,14 +1,27 @@
 from shutil import copyfile, which
 
+from exportimages import export_plotly
+
 from bidso.utils import replace_underscore
 from boavus.main import boavus
 from boavus.ieeg.dataset import Dataset
-from boavus.ieeg.preprocessing import preprocess_ecog
+from boavus.ieeg.preprocessing import preprocess_ecog, compute_frequency
+from boavus.ieeg import preprocessing
 
 from .paths import BIDS_PATH, task_ieeg, elec_ct, FREESURFER_PATH, BOAVUS_PATH, PARAMETERS_PATH, SIMULATE_PATH
 from .utils import update_parameters
 
+preprocessing.PARAMETERS['markers'].update({'on': 'move', 'off': 'rest', 'minimalduration': 20})
+preprocessing.PARAMETERS.update({'regions': ['ctx-lh-supramarginal', 'ctx-rh-superiorfrontal']})
 
 
-def test_ieeg_data():
-    assert True
+def test_ieeg_freq_percent():
+    ieeg_filename = task_ieeg.get_filename(BIDS_PATH)
+
+    fig = compute_frequency(ieeg_filename)
+
+    output_dir = task_ieeg.get_filename(BOAVUS_PATH).parent
+    output_dir.mkdir(exist_ok=True, parents=True)
+    export_plotly(fig, output_dir / 'output.png')
+
+
