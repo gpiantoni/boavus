bec19f2d9f8445827c3f10517646dcf82b52383b
Gio at UMCU <github@gpiantoni.com>
2018-05-18 15:10:46 +0200

    ecog compare

--- tests/test_06_ecog_compare.py
+++ tests/test_06_ecog_compare.py
@@ -12,8 +12,67 @@ from .paths import (BIDS_PATH,
 output_tsv = replace_underscore(task_ieeg.get_filename(ANALYSIS_PATH),
                                 'ieeg_compare.tsv')
 
+def test_ieeg_compare_diff():
+
+    boavus([
+        'ieeg',
+        'compare',
+        '--analysis_dir', str(ANALYSIS_PATH),
+        '--log', 'debug',
+        '--measure', 'diff',
+        ])
+
+    v = float([x['measure'] for x in read_tsv(output_tsv) if x['channel'] == 'grid01'][0])
+    print(v)
+    assert_allclose(v, 1.551890365962599)
+
 
 def test_ieeg_compare_percent():
+
+    boavus([
+        'ieeg',
+        'compare',
+        '--analysis_dir', str(ANALYSIS_PATH),
+        '--log', 'debug',
+        '--measure', 'percent',
+        ])
+
+    v = float([x['measure'] for x in read_tsv(output_tsv) if x['channel'] == 'grid01'][0])
+    print(v)
+    assert_allclose(v, 22.123573)
+
+
+def test_ieeg_compare_dorat():
+
+    boavus([
+        'ieeg',
+        'compare',
+        '--analysis_dir', str(ANALYSIS_PATH),
+        '--log', 'debug',
+        '--measure', 'dora_t',
+        ])
+
+    v = float([x['measure'] for x in read_tsv(output_tsv) if x['channel'] == 'grid01'][0])
+    print(v)
+    assert_allclose(v, -62.086533)
+
+
+def test_ieeg_compare_baseline():
+
+    boavus([
+        'ieeg',
+        'compare',
+        '--analysis_dir', str(ANALYSIS_PATH),
+        '--log', 'debug',
+        '--baseline',
+        ])
+
+    v = float([x['measure'] for x in read_tsv(output_tsv) if x['channel'] == 'grid01'][0])
+    print(v)
+    assert_allclose(v, 0.17740472587278314)
+
+
+def test_ieeg_compare():
 
     boavus([
         'ieeg',
@@ -23,6 +82,7 @@ def test_ieeg_compare_percent():
         ])
 
     v = float([x['measure'] for x in read_tsv(output_tsv) if x['channel'] == 'grid01'][0])
+    print(v)
     assert_allclose(v, 0.9309301804179754)
 
 
