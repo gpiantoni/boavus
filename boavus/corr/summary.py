406b84a711dbae28fbe81e3c672783ac9065c941
Gio at UMCU <github@gpiantoni.com>
2018-10-05 14:08:07 +0200

    copy files to summary

--- boavus/corr/summary.py
+++ boavus/corr/summary.py
@@ -1,4 +1,9 @@
+from shutil import copy
 
 
-def collect_corr():
-    pass
+def collect_corr(in_files, output_dir):
+
+    output_dir.mkdir(parents=True, exist_ok=True)
+
+    for in_file in in_files:
+        copy(in_file, output_dir)
