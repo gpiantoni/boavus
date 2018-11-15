097b6a8ff43d6566a79275f1d8e66d05271c283a
Gio on ArchXPS <github@gpiantoni.com>
2018-11-15 10:26:37 +0100

    use clinicalctmr

--- tests/paths.py
+++ tests/paths.py
@@ -40,6 +40,6 @@ elec = file_Core(
     subject=subject,
     session='UMCUECOGday01',
     modality='electrodes',
-    acquisition='clinical',
+    acquisition='clinicalctmr',
     extension='.tsv',
     )
