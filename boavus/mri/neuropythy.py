0d669406d626b5f6d2636e4747081ece2568a467
Gio at UMCU <github@gpiantoni.com>
2019-01-21 13:34:25 +0100

    specify subjects_dir

--- boavus/mri/neuropythy.py
+++ boavus/mri/neuropythy.py
@@ -1,9 +1,13 @@
 from nipype import Function
 
 
-def wrapper_neuropythy_atlas(subject_id):
+def wrapper_neuropythy_atlas(subject_id, subjects_dir=None):
     from pathlib import Path
     from neuropythy.commands import atlas
+    from os import environ
+
+    if subjects_dir is not None:
+        environ['SUBJECTS_DIR'] = subjects_dir
 
     output_path = str(Path('.').resolve() / subject_id)
 
@@ -21,6 +25,7 @@ def wrapper_neuropythy_atlas(subject_id):
 function_neuropythy_atlas = Function(
     input_names=[
         'subject_id',
+        'subjects_dir',
     ],
     output_names=[
         'output_dir',
--- tests/test_10_node_neuropythy.py
+++ tests/test_10_node_neuropythy.py
@@ -1,11 +1,12 @@
 from nipype import Node
 from boavus.mri.neuropythy import function_neuropythy_atlas
 
-from .paths import ANALYSIS_PATH
+from .paths import ANALYSIS_PATH, FREESURFER_PATH
 
 
 def test_neuropythy_atlas():
     n = Node(function_neuropythy_atlas, 'atlas')
-    n.base_dir = str(ANALYSIS_PATH / 'neuropythy')
+    n.base_dir = str(ANALYSIS_PATH)
     n.inputs.subject_id = 'sub-delft'
+    n.inputs.subjects_dir = str(FREESURFER_PATH)
     n.run()
